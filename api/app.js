c471cb41468c6af3ccef95d96e9014a59105e40e
Nazarii <nazariikubik@gmail.com>
2022-06-28 19:49:25 +0200

    init docker

---
+++ api/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json([
+    {
+      id: "1",
+      title: "Hello Breakfast",
+    },
+    {
+      id: "2",
+      title: "Hello Lunch",
+    },
+    {
+      id: "3",
+      title: "Hello Dinner",
+    },
+  ]);
+});
+
+app.listen(4000, () => console.log("The app is listening on port " + 4000));
